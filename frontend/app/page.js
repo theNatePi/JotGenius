23a87e41499f673935d8380a114bdd1d3483042d
Nathan Pietrantonio <pietrann@uci.edu>
2024-04-19 20:03:15 -0700

    Add DaisyUI

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -12,6 +12,17 @@ export default function Home() {
     <main className="flex flex-col items-center justify-between p-24">
       <h1 className="pb-20">TESTING</h1>
       <button className="bg-white text-black" onClick={() => router.push('/login')}>LOGIN</button>
+
+      <div className="card w-96 bg-base-100 shadow-xl">
+      <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+      <div className="card-body">
+        <h2 className="card-title">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+    </div>
     </main>
   );
 }
