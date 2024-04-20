c98c82b789bdae0788b81b4f42cda74410e7bcf9
Nathan Pietrantonio <pietrann@uci.edu>
2024-04-19 20:47:13 -0700

    Add primary font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -12,8 +12,11 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        main: ["Roboto Mono", "monospace"]
+      },  
       colors: {
-
+        primary: 'blue',
       },
     },
   },
