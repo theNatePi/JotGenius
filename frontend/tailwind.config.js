0ebf6d3c084a9a1f4c8ab9a08d787f42a252f7a1
Nathan Pietrantonio <pietrann@uci.edu>
2024-04-19 21:25:02 -0700

    Add scrolling cards

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,9 @@
 const {nextui} = require('@nextui-org/theme');
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette")
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -9,6 +14,17 @@ module.exports = {
   ],
   theme: {
     extend: {
+      animation: {
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+      },
+      keyframes: {
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
@@ -25,5 +41,17 @@ module.exports = {
       },
     },
   },
-  plugins: [require('daisyui'),nextui()],
+  darkMode: "class",
+  plugins: [require('daisyui'), nextui(), addVariablesForColors],
 };
+
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
