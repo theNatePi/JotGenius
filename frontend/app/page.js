3b06cd9d2ac67bdb9383c4b0eb1ecad5610906f6
Nathan Pietrantonio <pietrann@uci.edu>
2024-04-19 19:44:28 -0700

    Add login button

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,9 +1,17 @@
+'use client'
+
 import Image from "next/image";
+import { useRouter } from 'next/navigation';
+
+
 
 export default function Home() {
+  const router = useRouter();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-#000000">
-      <h1>TESTING</h1>
+    <main className="flex flex-col items-center justify-between p-24">
+      <h1 className="pb-20">TESTING</h1>
+      <button className="bg-white text-black" onClick={() => router.push('/login')}>LOGIN</button>
     </main>
   );
 }
