d15d2c2c9ee556144a776fcda6f36e09e9e42692
huynh-kyle23 <huynhkyle23@gmail.com>
2024-04-20 21:33:06 -0700

    Fixed database file merge.

--- backend/database.py
+++ backend/database.py
@@ -20,14 +20,35 @@ def attemptLogin(username, password):
     except ClientResponseError as err:
         login_data = {
             "code": err.status,
-            "message": err.data['identity']['message']
+            "message": err.data
+        }
+        return jsonify(login_data)
+
+
+def attemptSignUp(email, password, password_conf, first, last):
+    user_split = email.split('@')
+    signup_data = {
+        "username": user_split[0],
+        "email": email,
+        "emailVisibility": True,
+        "password": password,
+        "passwordConfirm": password_conf,
+        "first_name": first,
+        "last_name": last,
+        "note_library": {"notes": []}
+    }
+    try:
+        CLIENT.collection('jot_users').create(signup_data)
+        result_data = {
+            "code": 200,
+            "message": "Sign up successful."
         }
         return jsonify(result_data)
     except ClientResponseError as err:
         if err.status == 400:
             result_data = {
                 "code": 400,
-                "message": err.data['identity']['message']
+                "message": err.data
             }
             return jsonify(result_data)
         elif err.status == 403:
@@ -55,32 +76,7 @@ def attemptNoteUpload(user_id, content, video_id):
         if err.status == 400:
             note_data = {
                 "code": 400,
-                "message": err.data['identity']['message']
-            }
-            return jsonify(note_data)
-        elif err.status == 403:
-            note_data = {
-                "code": 403,
-                "message": err.data['message']
-            }
-            return jsonify(note_data)
-
-# giveNoteToUser('l3kwmiryni8i0il', 'l3kwmiryni8i0il')
-
-
-def attemptNoteUpload(user_id, content, video_id):
-    note_data = {
-        "note_content": content,
-        "video_id": video_id
-    }
-    try:
-        note_data = CLIENT.collection('jot_notes').create(note_data)
-
-    except ClientResponseError as err:
-        if err.status == 400:
-            note_data = {
-                "code": 400,
-                "message": err.data['identity']['message']
+                "message": err.data
             }
             return jsonify(note_data)
         elif err.status == 403:
