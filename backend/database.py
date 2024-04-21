288bd1ddeba77a89798db924db1450d4dde01593
huynh-kyle23 <huynhkyle23@gmail.com>
2024-04-21 01:36:57 -0700

    Added more backend logic.

--- backend/database.py
+++ backend/database.py
@@ -8,22 +8,24 @@ import json
 CLIENT = PocketBase('http://127.0.0.1:8090')
 
 
-def attemptLogin(username, password):
+def attemptLogin(email, password):
     try:
         received = CLIENT.collection(
-            'jot_users').auth_with_password(username, password)
+            'jot_users').auth_with_password(email, password)
         login_data = {
             "code": 200,
-            "message": "Login successful.",
+            "message": 'Login successful.',
             "user_id": received.record.id
         }
         return jsonify(login_data)
     except ClientResponseError as err:
-        login_data = {
-            "code": err.status,
-            "message": err.data
-        }
-        return jsonify(login_data)
+        if err.status == 400:
+            login_data = {
+                "code": 400,
+                "message": err.data,
+                "user_id": ''
+            }
+            return jsonify(login_data)
 
 
 def attemptSignUp(email, password, password_conf, first, last):
@@ -36,81 +38,176 @@ def attemptSignUp(email, password, password_conf, first, last):
         "passwordConfirm": password_conf,
         "first_name": first,
         "last_name": last,
-        "note_library": {"notes": []}
+        "note_library": {'notes': []}
     }
     try:
-        CLIENT.collection('jot_users').create(signup_data)
+        received = CLIENT.collection('jot_users').create(signup_data)
         result_data = {
             "code": 200,
-            "message": "Sign up successful."
+            "message": 'Sign up successful.',
+            "user_id": received.id
         }
         return jsonify(result_data)
     except ClientResponseError as err:
         if err.status == 400:
             result_data = {
                 "code": 400,
-                "message": err.data
+                "message": err.data,
+                "user_id": ''
             }
             return jsonify(result_data)
         elif err.status == 403:
             result_data = {
                 "code": 403,
-                "message": err.data['message']
+                "message": err.data['message'],
+                "user_id": ''
             }
             return jsonify(result_data)
 
 
 def giveNoteToUser(user_id, note_id):
     try:
-        user_data = CLIENT.collection('jot_users').get_one(user_id)
-        note_string = user_data.note_library
+        user_return = CLIENT.collection('jot_users').get_one(user_id)
+        note_string = user_return.note_library
         note_dict = json.loads(note_string)
         new_notes = list(note_dict['notes']).append(note_id)
         CLIENT.collection('jot_users').update(user_id, {'notes': new_notes})
         note_data = {
             "code": 200,
-            "message": "Successfully added note."
+            "message": 'Note added successfully.',
+            "note_id": note_id
         }
         return jsonify(note_data)
     except ClientResponseError as err:
         if err.status == 404:
             note_data = {
                 "code": 404,
-                "message": err.message
+                "message": err.message,
+                "note_id": ''
             }
             return jsonify(note_data)
         elif err.status == 400:
             note_data = {
                 "code": 400,
-                "message": err.data
-            }
-            return jsonify(note_data)
-        elif err.status == 403:
-            note_data = {
-                "code": 404,
-                "message": err.message
-            }
-            return jsonify(note_data)
-
-
-def attemptNoteUpload(user_id, content, video_id):
-    note_data = {
-        "note_content": content,
-        "video_id": video_id
-    }
-    try:
-        note_data = CLIENT.collection('jot_notes').create(note_data)
-        return giveNoteToUser(user_id, note_data.id)
-    except ClientResponseError as err:
-        if err.status == 400:
-            note_data = {
-                "code": 400,
-                "message": err.data
+                "message": err.data,
+                "note_id": ''
             }
             return jsonify(note_data)
         elif err.status == 403:
             note_data = {
                 "code": 403,
-                "message": err.data['message']
+                "message": err.message,
+                "note_id": ''
             }
             return jsonify(note_data)
+
+
+def attemptNoteUpload(user_id, content, video_id, score, feedback):
+    note_info = {
+        "note_content": content,
+        "video_id": video_id,
+        "score": score,
+        "feedback": feedback
+    }
+    try:
+        received = CLIENT.collection('jot_notes').create(note_info)
+        return giveNoteToUser(user_id, received.id)
+    except ClientResponseError as err:
+        if err.status == 400:
+            note_data = {
+                "code": 400,
+                "message": err.data,
+                "note_id": ''
+            }
+            return jsonify(note_data)
+        elif err.status == 403:
+            note_data = {
+                "code": 403,
+                "message": err.data['message'],
+                "note_id": ''
+            }
+            return jsonify(note_data)
+
+
+def accessNoteInfo(note_id):
+    try:
+        note_return = CLIENT.collection('jot_notes').get_one(note_id)
+        note_data = {
+            "code": 200,
+            "note_id": note_return.id,
+            "video_id": note_return.video_id,
+            "note_content": note_return.note_content,
+            "score": note_return.score,
+            "feedback": note_return.feedback
+        }
+        return jsonify(note_data)
+    except ClientResponseError as err:
+        if err.status == 404:
+            note_data = {
+                "code": 404,
+                "message": err.message,
+                "note_id": ''
+            }
+            return jsonify(note_data)
+        elif err.status == 400:
+            note_data = {
+                "code": 400,
+                "message": err.data,
+                "note_id": ''
+            }
+            return jsonify(note_data)
+        elif err.status == 403:
+            note_data = {
+                "code": 403,
+                "message": err.message,
+                "note_id": ''
+            }
+            return jsonify(note_data)
+
+
+def attemptVideoUpload(video_id, transcription):
+    vid_info = {
+        "video_id": video_id,
+        "transcription": transcription
+    }
+    try:
+        received = CLIENT.collection('jot_videos').create(vid_info)
+        vid_data = {
+            "code": 200,
+            "message": 'Video uploaded successfully.'
+        }
+        return jsonify(vid_data)
+    except ClientResponseError as err:
+        if err.status == 400:
+            vid_data = {
+                "code": 400,
+                "message": err.data
+            }
+            return jsonify(vid_data)
+        elif err.status == 403:
+            vid_data = {
+                "code": 403,
+                "message": err.data['message']
+            }
+            return jsonify(vid_data)
+
+
+def accessVideoIDs():
+    try:
+        results = CLIENT.collection(
+            'jot_videos').get_full_list({'sort': '-created'})
+        id_list = []
+        for result in results:
+            id_list.append(result.id)
+        vid_data = {
+            "code": 200,
+            "video_ids": id_list
+        }
+        return jsonify(vid_data)
+    except ClientResponseError as err:
+        if err.status == 400:
+            vid_data = {
+                "code": 400,
+                "message": err.message,
+            }
+            return jsonify(vid_data)
